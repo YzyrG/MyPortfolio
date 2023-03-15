a4c1322db2259ae6fd22eda6c2aa55fe12564b74
Zyr <zyr724725@gmail.com>
2023-03-15 22:45:10 +0800

    修改 csv文件

--- Home.py
+++ Home.py
@@ -1,23 +1,29 @@
 """
  My Portfolio home page
 """
-
+import urllib3
 import streamlit as st
 import pandas  # 用来读data.csv
 import requests
 
 # ----------------------------------------首页展示NASA图片-----------------------------------------------#
 # 通过api拿到图片json文件并读取
-api_key = "koefKvttCwHoobdue0TJBno3Q1wgwi9Lg2urCDeM"
+api_key = "tnsxcGgQyfXgTMwhPTlBgSDBt0LNKyNOmzsr6FZd"
 url = "https://api.nasa.gov/planetary/apod?" \
       f"api_key={api_key}"
+headers = {'Connection': 'close'}
+
+# 不显示InsecureRequestWarning警告
+urllib3.disable_warnings()
 
 # 请求页面
-request = requests.get(url)
+request = requests.get(url, headers=headers, verify=False)
 content_1 = request.json()
-
+print(content_1)
 # 读取json
 date = content_1["date"]
+title = content_1['title']
+img_copyright = content_1['copyright']
 img_url = content_1["url"]
 explanation = content_1["explanation"]
 
@@ -38,21 +44,24 @@ st.set_page_config(layout="wide", page_title="My Python Portfolio | ZYR ", page_
 # 显示NASA图片
 st.image(img_filepath)
 
-# 显示自我介绍
-content_1 = """
-Hi!我叫ZYR, 是一名编程学习者，目前正在学习python语言&练习一些小项目。
-我于2019年毕业于西南大学商贸学院信息管理与信息系统专业，
-曾在上海泛微网络科技有限公司担任测试工程师的角色，
-主要负责公司产品e-cology系统公文模块的功能测试任务。
-但相比测试我更想倾向于做一名编程者，所以正在因此而努力:)
-"""
-st.info(content_1)
+# 图片来源说明
+st.text(f"{title} by {img_copyright} from NASA Astronomy Picture of the Day.")
 
 # 作品展示
 content_2 = """
-    欢迎参观我的个人网站，下面是我做过的一些Python项目。\n
+    欢迎来到这里，祝您冲浪愉快！o(*￣︶￣*)o\n
     """
-st.subheader(content_2)
+st.header(content_2)
+
+# 显示自我介绍
+content_1 = """
+嗨！我叫ZYR, 是一名编程学习者，目前正在学习python语言&练习一些小项目。
+我于2019年毕业于西南大学商贸学院信息管理与信息系统专业，
+曾在上海泛微网络科技有限公司担任测试工程师的角色，
+主要负责公司产品e-cology系统公文模块的功能测试任务。
+但相比测试我更想倾向于做一名可以自己开发的编程者，所以正在因此而努力:)\n\n
+"""
+st.info(content_1)
 
 # 使用Pandas读取data.csv
 data = pandas.read_csv("data.csv", sep=';')
@@ -60,7 +69,7 @@ column3, column4 = st.columns(2)
 
 with column3:
     #  iterrows()使得各行数据与第一行相应类别对应起来
-    for index, row in data[:10].iterrows():
+    for index, row in data[:4].iterrows():
         st.subheader(row["title"])
         st.write(row['description'])
         st.image(f"Images/{row['image']}", width=300)
@@ -68,7 +77,7 @@ with column3:
         st.write(f"[source code]({row['url']})")
 
 with column4:
-    for index, row in data[10:].iterrows():
+    for index, row in data[4:].iterrows():
         st.subheader(row['title'])
         st.write(row['description'])
         st.image(f"Images/{row['image']}", width=300)
